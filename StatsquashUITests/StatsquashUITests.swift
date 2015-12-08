6f2899215cf6b0fed8a3b72bbaa2743a47f069c9
tuvaneck <tuvaneck@students.hackleyschool.org>
2015-12-08 08:50:49 -0500

    Initial Commit

---
+++ StatsquashUITests/StatsquashUITests.swift
@@ -0,0 +1,36 @@
+//
+//  StatsquashUITests.swift
+//  StatsquashUITests
+//
+//  Created by Tucker van Eck on 12/8/15.
+//  Copyright © 2015 Tucker van Eck and Akira Shindo. All rights reserved.
+//
+
+import XCTest
+
+class StatsquashUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
